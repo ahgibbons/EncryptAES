e2c9c7566ee105fb2a7ad3d941141ba4114dbdde
Andrew.Windows <andrew.h.gibbons@gmail.com>
2017-09-06 11:42:00 +0900

    Implemented with cryptonite package as a control

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,18 @@ import qualified Data.ByteString as BS
 import Data.Maybe (fromJust)
 import Data.HexString
 
+import Crypto.Cipher.AES
+import Crypto.Cipher.Types
+import Crypto.Error
+
 import Tests (tkey,tiv)
+
+import Utils (pkcs7, unpkcs7)
+{-}
 import Rjindael
 import Types
 import Block (cbcEncrypt, cbcDecrypt)
+-}
 
 main :: IO ()
 main = do
@@ -18,10 +26,12 @@ main = do
     let flag    = args !! 0
         fpath   = args !! 1
         outpath = args !! 2
+        CryptoPassed cipher = cipherInit tkey :: CryptoFailable AES128
+        Just iv             = makeIV tiv :: (Maybe (IV AES128))
     intext <- BS.readFile fpath
     let outtext = case flag of 
-                   "-e" ->  cbcEncrypt AES128 tiv tkey intext 
-                   "-d" ->  fromJust . cbcDecrypt AES128 tkey $ intext
+                   "-e" ->  cbcEncrypt cipher iv (pkcs7 16 intext)
+                   "-d" ->  fromJust. unpkcs7 . cbcDecrypt cipher iv $ intext
     BS.writeFile outpath outtext
     putStrLn "Finished."
 

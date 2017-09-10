08f647158b56af929e576e47cdb545439c5f9f70
Andrew <Andrew@li-ys-MacBook-Pro.local>
2017-09-10 09:16:01 +0900

    Implemented quickMult using precomputed vectors.

--- src/GaloisFields.hs
+++ src/GaloisFields.hs
@@ -7,6 +7,8 @@ module GaloisFields where
 import Data.Bits
 import Data.Word
 import Data.Matrix
+import Data.Maybe
+import qualified Data.Vector.Storable as VS
 
 infixl 7 .*.
 infixl 6 .+.
@@ -28,7 +30,8 @@ instance Num (GF2 Int) where
 
 instance Num (GF2 Word8) where
     (GF2 a) + (GF2 b) = GF2 (a `xor` b)
-    (GF2 a) * (GF2 b) = GF2 (multGF2 aes_GF2_mod a b)
+--    (GF2 a) * (GF2 b) = GF2 (multGF2 aes_GF2_mod a b)
+    (GF2 a) * (GF2 b) = GF2 $ quickMult a b
     negate (GF2 a)    = GF2 a
     signum _          = GF2 1
     abs a             = a
@@ -46,7 +49,8 @@ instance GaloisField2 Int where
 
 instance GaloisField2 Word8 where
   addGF2 a b = a `xor` b
-  multGF2 = multGF2Word8
+  --multGF2 = multGF2Word8
+  multGF2 n a b = quickMult a b
 
 (.*.) :: GaloisField2 a => a -> a -> a
 (.*.) a b = multGF2 aes_GF2_mod a b
@@ -114,6 +118,22 @@ orbit n = 1 : orbit' (n)
  where
    orbit' a
      | a == 1 = []
-     | otherwise = a : orbit' (a .*. n) 
+     | otherwise = a : orbit' (multGF2Word8 283 a n) 
 
 
+gf2Generator :: VS.Vector Word8
+gf2Generator = VS.fromList $ orbit 3
+
+logGF2Generator :: VS.Vector Word8
+logGF2Generator = VS.fromList $ fmap logGF2 [1..255]
+  where
+    logGF2 n = fromIntegral . fromJust
+             . VS.findIndex (==n) $ gf2Generator
+
+quickMult :: Word8 -> Word8 -> Word8
+quickMult 0 _ = 0
+quickMult _ 0 = 0
+quickMult a b = gf2Generator VS.! ((n+m) `mod` 255)
+  where
+    n = fromIntegral $ logGF2Generator VS.! (fromIntegral a - 1)
+    m = fromIntegral $ logGF2Generator VS.! (fromIntegral b - 1)
